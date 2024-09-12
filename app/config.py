bc7553537219a66d204c2b32fd377c3fe89a4ef7
YASHALAWA8 <95289183+YASHALAWA8@users.noreply.github.com>
2024-09-12 11:19:59 +0300

    env

--- app/config.py
+++ app/config.py
@@ -4,7 +4,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file=".env_copy")
+    model_config = SettingsConfigDict(env_file=".env")
 
     MODE: Literal["DEV", "TEST", "PROD"] = "DEV"
 
